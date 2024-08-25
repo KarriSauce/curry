893a8c653f876034b84febfeb4e808c7d15b642d
Curry <65170188+KarriSauce@users.noreply.github.com>
2024-08-25 12:19:36 -0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   blog: {
     title: "katia taras",
     description: "welcome",
-    scheme: "dark", // 'light' | 'dark' | 'system'
+    scheme: "system", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
